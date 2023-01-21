e4d8279940abc53fdce1d4d296dd2328467f2dec
BeatedHub <121546621+BeatedHub@users.noreply.github.com>
2023-01-21 14:01:06 +0100

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,3 +1,3 @@
 --// please subscribe pls!
 --// It will freeze your game for some seconds
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BeatedHub/FireTeam/main/File.lua"))() print("Made by RevertUser! .gg/W7YuHcVCqH")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BeatedHub/FireTeam/main/File.lua"))(print("Made by RevertUser! .gg/W7YuHcVCqH"))
